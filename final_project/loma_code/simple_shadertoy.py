05bdc2cf169a8432b009d6c7e41e2e70993a1b64
Skyler <yiz249@ucsd.edu>
2024-05-22 11:45:19 -0700

    add simple shader toy for one color

--- final_project/loma_code/simple_shadertoy.py
+++ final_project/loma_code/simple_shadertoy.py
@@ -10,34 +10,37 @@ def make_vec3(x : In[float], y : In[float], z : In[float]) -> Vec3:
     ret.z = z
     return ret
 
-def shadertoy(img : In[Array[Vec3]]) -> Array[Vec3]:
-    return img
+def shadertoy(col: In[Vec3])-> Vec3:
+    return col
 
 d_shadertoy = fwd_diff(shadertoy)
 
 
-def diff_shadertoy(image:In[Array[Vec3]], w : In[int], h : In[int], diff_image : Out[Array[Vec3]]):
+def diff_shadertoy(col: In[Vec3], w : In[int], h : In[int])->Vec3:
     y : int = 0
     x : int
-    d_image : Array[Diff[Vec3],10000]
-    d_color : Array[Diff[Vec3],10000]
+    d_color : Diff[Vec3]
+    d_col: Diff[Vec3]
+    loss: Vec3
     while (y < h, max_iter := 4096):
         x = 0
         while (x < w, max_iter := 4096):
-            d_image[w * y + x].val = image.val
-            d_image[w * y + x].dval = 1
+            d_col.x.val = col.x
+            d_col.y.val = col.y
+            d_col.z.val = col.z
+            d_col.x.dval = 1
+            d_col.y.dval = 1
+            d_col.z.dval = 1
+            d_color = d_shadertoy(d_col)
+            loss.x = loss.x+d_color.x.dval*2*(d_color.x.val-0.4)
+            loss.y = loss.y+d_color.y.dval*2*(d_color.y.val-0.8)
+            loss.z = loss.z+d_color.z.dval*2*(d_color.z.val-0.6)
             x = x + 1
         y = y + 1
-    y = 0
-    while (y < h, max_iter := 4096):
-        x = 0
-        while (x < w, max_iter := 4096):
-            d_color = d_shadertoy(d_image)
-            diff_image[w * y + x].x = d_color[w * y + x].x.dval*2*(image[w * y + x].x-0.4)
-            diff_image[w * y + x].y = d_color[w * y + x].y.dval*2*(image[w * y + x].y-0.8)
-            diff_image[w * y + x].z = d_color[w * y + x].z.dval*2*(image[w * y + x].z-0.6)
-            x = x + 1
-        y = y + 1
+    loss.x = loss.x/(w*h)
+    loss.y = loss.y/(w*h)
+    loss.z = loss.z/(w*h)
+    return loss
 
 
 
--- final_project/simple_shadertoy_host.py
+++ final_project/simple_shadertoy_host.py
@@ -13,19 +13,20 @@ if __name__ == '__main__':
         structs, lib = compiler.compile(f.read(),
                                   target = 'c',
                                   output_filename = '_code/simple_shadertoy')
-    w = 400
-    h = 225
-    loss = np.zeros([h, w, 3], dtype = np.single)
-    img = np.ones([h, w, 3], dtype = np.single)
+    w = 100
+    h = 100
     x = 0.5
     y = 0.5
     z = 0.5
     grad_f = lib.diff_shadertoy
     step_size = 1e-2
-    for i in range(100):
-        grad_f(img.ctypes.data_as(ctypes.POINTER(structs['Vec3'])),w, h, loss.ctypes.data_as(ctypes.POINTER(structs['Vec3'])))
-        for j in range(w):
-            for k in range(h):
-                img[0].val -= step_size * loss[0].val
+    d_vec3 = structs["Vec3"]
+    for i in range(1000):
+        loss = grad_f(d_vec3(x,y,z),w, h)
+        x -= step_size * loss.x
+        y -= step_size * loss.y
+        z -= step_size * loss.z
+
+
 
     

e1072a6fe4e6c32880325a9072fa5e7501af9c91
angelajc1 <42723603+angelajc1@users.noreply.github.com>
2024-05-27 18:06:34 -0700

    WIP changes

--- final_project/circle_shadertoy_host.py
+++ final_project/circle_shadertoy_host.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     fig_row = 10
     fig_col = 5
     fig, axes = plt.subplots(fig_row, fig_col, figsize=(8, 8))
-    for i in range(1000):
+    for i in range(10000):
         loss = ctypes.c_float(0.0)
         gradient = grad_f(w, h, cur_radius, target_radius, cur_center, target_center, img_x.ctypes.data_as(ctypes.POINTER(structs['Vec3'])), img_y.ctypes.data_as(ctypes.POINTER(structs['Vec3'])),loss)
         #cur_radius -= step_size * (gradient.x + gradient.y + gradient.z) / 3
@@ -50,32 +50,29 @@ if __name__ == '__main__':
             print("cur_radius at iteration " + str(i) + " is " + str(cur_radius))
             print("cur_center is (" + str(cur_center.x) + ", " + str(cur_center.y) + ", " + str(cur_center.z) + ")")
             print("gradient is " + str(gradient.x) + ", " + str(gradient.y) + ", " + str(gradient.z))
-        if i % 10 == 0 and i < 500:
-            j = i // 10
+        if i % 200 == 0:
+            j = i // 200
             axes[j // fig_col, j % fig_col].imshow(img_x)
             axes[j // fig_col, j % fig_col].axis("off")
             axes[j // fig_col, j % fig_col].set_title("i = " + str(i))
     #plt.subplots_adjust(wspace=0.4, hspace=0.4)
     plt.show()
 
-    iterations = list(range(1000))
-
-    # Plot center
+    iterations = list(range(10000))
     plt.figure(figsize=(10, 6))
     plt.plot(iterations, center_x, label='center x', color='blue', linewidth=2)
     plt.plot(iterations, center_y, label='center y', color='red', linewidth=2)
-    plt.title('Change in Center Over Iterations')
+    # plt.plot(iterations, radii, label='radius', color='red', linewidth=2)
+    plt.title('Radius Over Iterations')
     plt.xlabel('Iterations')
-    plt.ylabel('Center Coordinates')
+    plt.ylabel('Radius')
     plt.legend()
 
     plt.grid(True)
     plt.show()
 
-    # # Plot radius
-    # plt.figure(figsize=(10, 6))
-    # plt.plot(iterations, radii, label='radius', color='red', linewidth=2)
-
+    plt.figure(figsize=(10, 6))
+    plt.plot(iterations, losses, label='Loss', color='red', linewidth=2)
     plt.title('Loss Function Over Iterations')
     plt.xlabel('Iterations')
     plt.ylabel('Loss Value')

babd90e8579a2995a908c40af12c7e2551ecc44d
angelajc1 <42723603+angelajc1@users.noreply.github.com>
2024-05-27 17:25:49 -0700

    Minor edits to circle host

--- final_project/circle_shadertoy_host.py
+++ final_project/circle_shadertoy_host.py
@@ -8,14 +8,6 @@ import ctypes
 import numpy as np
 import matplotlib.pyplot as plt
 
-def draw_circle(w, h, center_x, center_y, radius):
-    circle_color = [0, 0, 1] # blue
-    img = np.ones((w, h, 3)) # white background
-    y, x = np.ogrid[:w, :h]
-    distance_from_center = np.sqrt((x - center_x)**2 + (y - center_y)**2)
-    mask = (distance_from_center <= radius)
-    img[mask] = circle_color 
-    return img
 
 if __name__ == '__main__':
     with open('loma_code/circle_shadertoy.py') as f:
@@ -71,14 +63,35 @@ if __name__ == '__main__':
     plt.show()
 
     iterations = list(range(1000))
+
+    # Plot center
     plt.figure(figsize=(10, 6))
     plt.plot(iterations, center_x, label='center x', color='red', linewidth=2)
     plt.plot(iterations, center_y, label='center y', color='red', linewidth=2)
+    plt.title('Change in Center Over Iterations')
+    plt.xlabel('Iterations')
+    plt.ylabel('Center Coordinates')
+    plt.legend()
+
+    plt.grid(True)
+    plt.show()
+
+    # # Plot radius
+    # plt.figure(figsize=(10, 6))
     # plt.plot(iterations, radii, label='radius', color='red', linewidth=2)
-    # plt.plot(iterations, loss_r, label='R Loss', color='red', linewidth=2)
-    # plt.plot(iterations, loss_g, label='G Loss', color='green', linewidth=2)
-    # plt.plot(iterations, loss_b, label='B Loss', color='blue', linewidth=2)
-    # 添加标题和标签
+    # plt.title('Change in Radius Over Iterations')
+    # plt.xlabel('Iterations')
+    # plt.ylabel('Radius')
+    # plt.legend()
+
+    # plt.grid(True)
+    # plt.show()
+
+    # Plot loss
+    plt.figure(figsize=(10, 6))
+    plt.plot(iterations, loss_r, label='R Loss', color='red', linewidth=2)
+    plt.plot(iterations, loss_g, label='G Loss', color='green', linewidth=2)
+    plt.plot(iterations, loss_b, label='B Loss', color='blue', linewidth=2)
     plt.title('Loss Function Over Iterations')
     plt.xlabel('Iterations')
     plt.ylabel('Loss Value')
@@ -86,41 +99,4 @@ if __name__ == '__main__':
 
     plt.grid(True)
     plt.show()
-
-    # w = 100
-    # h = 100
-    # cur_img = draw_circle(w, h, center_x=50, center_y=50, radius=10)
-    # target_img = draw_circle(w, h, center_x=50, center_y=50, radius=20)
-    # grad_f = lib.diff_shadertoy
-    # step_size = 1e-2
-    # d_vec3 = structs["Vec3"]
-    # fig_row = 6
-    # fig_col = 5
-    # fig, axes = plt.subplots(fig_row, fig_col, figsize=(8, 8))
-    # axes[fig_row-1, 0].imshow(cur_img)
-    # axes[fig_row-1, 0].axis("off")
-    # axes[fig_row-1, 0].set_title("Start image")
-    # axes[fig_row-1, 1].imshow(target_img)
-    # axes[fig_row-1, 1].axis("off")
-    # axes[fig_row-1, 1].set_title("Target image")
-
-    # for i in range(1000):
-    #     loss = np.zeros([h, w, 3], dtype = np.single)
-    #     grad_f(w, h, 
-    #            cur_img.ctypes.data_as(ctypes.POINTER(structs['Vec3'])),
-    #            target_img.ctypes.data_as(ctypes.POINTER(structs['Vec3'])), 
-    #            loss.ctypes.data_as(ctypes.POINTER(structs['Vec3'])))
-    #     cur_img -= step_size * loss
-    #     if i % 40 == 0:
-    #         j = i // 40
-    #         axes[j // fig_col, j % fig_col].imshow(cur_img)
-    #         axes[j // fig_col, j % fig_col].axis("off")
-    #         axes[j // fig_col, j % fig_col].set_title("i = " + str(i))
-    
-    # axes[fig_row-1, 2].imshow(cur_img)
-    # axes[fig_row-1, 2].axis("off")
-    # axes[fig_row-1, 2].set_title("1000 iterations")
-    # plt.subplots_adjust(wspace=0.4, hspace=0.4)
-    # plt.show()
-
 
